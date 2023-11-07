a773b0999452ef0400184e6890f92a814955f612
Tunglies <tunglies@outlook.com>
2023-11-08 00:05:15 +0800

    1108

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,8 +1,8 @@
 //! Process management syscalls
 use crate::{
     config::MAX_SYSCALL_NUM,
-    task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus},
-    timer::get_time_us,
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus, get_current_task},
+    timer::{get_time_us, get_time_ms},
 };
 
 #[repr(C)]
@@ -53,5 +53,23 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 /// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info");
-    -1
+    let task = get_current_task();
+    unsafe {
+        *_ti = TaskInfo {
+            // status: task.task_status,
+            status: TaskStatus::Running,
+            syscall_times: task.syscall_times,
+            time: get_time_ms() - task.start_time
+        };
+
+        debug!("task cost time: {:?}", (*_ti).time);
+
+        // for i in 0..500 {
+        //     if task.syscall_times[i] > 0 {
+        //         debug!("{:?}, {:?}", i, task.syscall_times[i]);
+        //     }
+        // }
+    }
+    // debug!("system end: {:?}", get_time_ms());
+    0
 }
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,9 +14,10 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
+use crate::timer::get_time_ms;
 use lazy_static::*;
 use switch::__switch;
 pub use task::{TaskControlBlock, TaskStatus};
@@ -54,6 +55,8 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            start_time: 0,
+            syscall_times: [0; MAX_SYSCALL_NUM]
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -79,7 +82,12 @@ impl TaskManager {
     fn run_first_task(&self) -> ! {
         let mut inner = self.inner.exclusive_access();
         let task0 = &mut inner.tasks[0];
-        task0.task_status = TaskStatus::Running;
+
+        if task0.start_time == 0 {
+            task0.start_time = get_time_ms();
+            task0.task_status = TaskStatus::Running;
+        }
+
         let next_task_cx_ptr = &task0.task_cx as *const TaskContext;
         drop(inner);
         let mut _unused = TaskContext::zero_init();
@@ -121,7 +129,12 @@ impl TaskManager {
         if let Some(next) = self.find_next_task() {
             let mut inner = self.inner.exclusive_access();
             let current = inner.current_task;
-            inner.tasks[next].task_status = TaskStatus::Running;
+
+            if inner.tasks[next].start_time == 0 {
+                inner.tasks[next].start_time = get_time_ms();
+                inner.tasks[next].task_status = TaskStatus::Running;
+            }
+
             inner.current_task = next;
             let current_task_cx_ptr = &mut inner.tasks[current].task_cx as *mut TaskContext;
             let next_task_cx_ptr = &inner.tasks[next].task_cx as *const TaskContext;
@@ -134,6 +147,24 @@ impl TaskManager {
         } else {
             panic!("All applications completed!");
         }
+    }
+
+    ///
+    pub fn get_current_task(&self) -> TaskControlBlock {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        let task = inner.tasks[current];
+        drop(inner);
+        task
+    }
+    ///
+    
+    /// 
+    pub fn increase_syscall_times(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_times[syscall_id] += 1;
+        drop(inner);
     }
 }
 
@@ -168,4 +199,17 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// ...
+pub fn get_current_task() -> TaskControlBlock {
+    TASK_MANAGER.get_current_task()
+}
+
+/// ...
+pub fn increase_syscall_times(syscall_id: usize) {
+    if syscall_id >= MAX_SYSCALL_NUM {
+        return ;
+    }
+    TASK_MANAGER.increase_syscall_times(syscall_id);
 }
